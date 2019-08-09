6ea49a23ad994ab6ec7c7daa2ad1f11488044b71
ttcom <1902305073@qq.com>
2019-08-09 15:55:50 +0800

    1555

--- application/api/service/UserToken.php
+++ application/api/service/UserToken.php
@@ -3,7 +3,33 @@
 
 class UserToken
 {
-    public function get($code){
+    protected $code;
+    protected $wxAppID;
+    protected $wxAppSecret;
+    protected $wxLoginUrl;
 
+    function __construct($code)
+    {
+        $this->code = $code;
+        $this->wxAppID = config('wx.app.id');
+        $this->wxAppSecret = config('wx.app_secret');
+        $this->wxLoginUrl = sprintf(config('wx.login_url'),
+            $this->wxAppID,$this->wxAppSecret,$this->code);
+
+    }
+
+    public function get($code){
+       $result = curl_get($this->wxLoginUrl);
+       $wxResult = json_decode($result,true);
+       if(empty($wxResult)){
+           throw new Exception('获取sessionkey是异常,微信内部错误');
+       }else{
+           $loginFail = array_key_exists('errcode',$wxResult);
+           if($loginFail){
+
+           }else{
+
+           }
+       }
     }
 }
