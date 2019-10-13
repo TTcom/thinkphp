85a67902c9f4895a323649aa2862c214ab5efac3
ttcom <1902305073@qq.com>
2019-10-13 22:32:02 +0800

    2231-13

--- application/api/controller/v1/Order.php
+++ application/api/controller/v1/Order.php
@@ -7,6 +7,7 @@ namespace app\api\controller\v1;
 use app\api\controller\BaseController;
 use app\api\validate\OrderPlace;
 use app\api\service\Token as TokenService;
+use app\api\service\Order as OrderService;
 class Order extends BaseController
 {
      // 用户在选择商品后，向API提交包含他所选择商品的相关信息
@@ -37,6 +38,8 @@ class Order extends BaseController
             (new OrderPlace())->goCheck();
             $products = input('post.products/a');
             $uid = TokenService::getCurrentUid();
-
+            $order = new OrderService();
+            $status = $order->place($uid,$products);
+            return $status;
         }
 }
--- application/api/service/Order.php
+++ application/api/service/Order.php
@@ -4,8 +4,11 @@
 namespace app\api\service;
 
 
+use app\api\model\OrderProduct;
 use app\api\model\Product;
 use app\lib\exception\OrderException;
+use app\lib\exception\userException;
+use think\Exception;
 
 class Order
 {
@@ -29,12 +32,94 @@ class Order
             return $status;
         }
         //开始创建订单
+        $orderSnap = $this->snapOrder($status);
+        $order = $this->createOrder($orderSnap);
+        $order['pass'] = true;
+        return $order;
+
+    }
+
+    private  function  createOrder($snap){  //生成订单数据
+        try{
+        $orderNo = $this->makeOrderNo($snap);  //生成订单号
+        $order = new \app\api\model\Order();
+        $order->user_id = $this->uid;
+        $order->order_no= $orderNo;
+        $order->total_price = $snap['orderPrice'];
+        $order->total_count = $snap['totalCount'];
+        $order->snap_img = $snap['snapImg'];
+        $order->snap_name = $snap['snapName'];
+        $order->snap_address = $snap['snapAddress'];
+        $order->snap_items = json_encode($snap['pStatus']);
+        $order->save(); //写入数据库
+
+        $orderID = $order->id;
+        $create_time = $order->create_time;
+        foreach ($this->oProducts as &$p){
+            $p['order_id'] = $orderID;
+        }
+        $orderProduct = new OrderProduct();
+        $orderProduct->saveAll($this->oProducts);
+        return [
+            'order_no' => $orderNo,
+            'order_id' => $orderID,
+            'create_time' => $create_time
+        ];
+         }
+         catch (Exception $ex){
+             throw $ex;
+         }
+    }
+    public static function makeOrderNo(){  // 生成订单号
+        $yCode = array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J');
+        $orderSn =
+            $yCode[intval(date('Y')) - 2017] . strtoupper(dechex(date('m'))) . date(
+                'd') . substr(time(), -5) . substr(microtime(), 2, 5) . sprintf(
+                '%02d', rand(0, 99));
+        return $orderSn;
+    }
+
+    //生成订单快照
+    private  function  snapOrder($status){
+
+        $snap = [
+          'orderPrice' => 0,
+          'totalCount' => 0,
+          'pStatus' => [],
+          'snapAddress'=> null,
+          'snapName' => '',
+          'snapImg'=>''
+        ];
+        $snap['orderPrice'] = $status['orderPrice'];
+        $snap['totalCount'] = $status['totalCount'];
+        $snap['pStatus'] = $status['pStatusArray'];
+        $snap['snapAddress'] = json_encode($this->getUserAddress());
+        $snap['snapName'] = $this->products[0]['name'];
+        $snap['snapImg'] = $this->products[0]['main_img_url'];
+
+        if(count($this->products)>1){
+            $snap['snapName'] .= '等';
+        }
+
+    }
+    private function  getUserAddress(){
+        $userAddress = UserAddress::where('user_id','=',$this->uid)
+            ->find();
+        if(!$userAddress){
+            throw new userException([
+                'msg' => '用户收货地址不存在，下单失败',
+                'errorCode' => 60001,
+            ]);
+        }
+        return $userAddress->toArray();
+
 
     }
     private function getOrderStatus(){     //对比商品信息
         $status = [
           'pass' => true,
           'orderPrice' => 0,          //商品价格总和
+            'totalCount' => 0,
             'pStatusArray'=>[],   //详细地商品信息
         ];
         foreach ($this->oProducts as $oProduct){
@@ -46,6 +131,7 @@ class Order
                  $status['pass'] = false;
              }
              $status['orderPrice']+= $pStatus['totalPrice'];
+            $status['totalCount']+= $pStatus['count'];
              array_push($status['pStatusArray'],$pStatus);
         }
         return $status;
