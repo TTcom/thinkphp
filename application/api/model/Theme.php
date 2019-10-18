f3328c0a2c11e78afa923db4a799b5178ac25e6c
ttcom <1902305073@qq.com>
2019-10-18 16:18:33 +0800

    1618

--- application/api/model/Theme.php
+++ application/api/model/Theme.php
@@ -6,7 +6,7 @@ namespace app\api\model;
 
 class Theme extends BaseModel
 {
-    protected $hidden = ['update_time','delete_time','id','topic_img_id','head_img_id'];
+    protected $hidden = ['update_time','delete_time','topic_img_id','head_img_id'];
     public function topicImg(){
         //belongsTo：一对一关系
         return $this->belongsTo('Image','topic_img_id','id');  //model关联image表，外键：topic_img_id；另一张表里的主键关联imaged的id
